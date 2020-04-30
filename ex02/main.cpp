92aa8f12e665b6b24729cf41bb021178148d5da5
fr0xy <rchoffar@student.42.fr>
2020-04-30 14:08:38 +0200

    first comit

---
+++ ex02/main.cpp
@@ -0,0 +1,27 @@
+#include "Fixed.class.hpp"
+#include <iostream>
+#include <iomanip>
+
+int     main(void) {
+    Fixed a;
+    Fixed const b(Fixed(5.05f) * Fixed(2));
+
+    std::cout << a << std::endl;
+    std::cout << std::boolalpha << (a < b) << std::endl;
+    std::cout << (a == b) << std::endl;
+    std::cout << (a != a) << std::endl;
+    std::cout << (b != 0) << std::endl;
+    std::cout << std::noboolalpha << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    
+    std::cout << Fixed::max(a, b) << std::endl;
+    std::cout << Fixed::min(a, b) << std::endl;
+        
+    return 0;
+}
